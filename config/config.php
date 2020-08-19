5aaa6b4987f82a17fdc0be7b7515ccc09475e8bc
OhenebaAduhene <aduhenewisdom97@gmail.com>
2020-08-19 22:03:33 +0000

    deployment

--- config/config.php
+++ config/config.php
@@ -1,9 +1,11 @@
 <?php
 
+
 //variables for DB connections
-define( 'DB_NAME', 'anglican' );
-define( 'DB_PWD', '' );
-define( 'DB_HOST', 'localhost' );
-define( 'DB_USER', 'root' );
+define( 'DB_NAME', 'heroku_8f63a60ee2bfb58' );
+
+define( 'DB_PWD', 'f370555b' );
+define( 'DB_HOST', 'us-cdbr-east-02.cleardb.com' );
+define( 'DB_USER', 'bafc2e19649bdb' );
 
 ?>
